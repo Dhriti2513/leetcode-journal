86d8e861fe68b95808d6e6ea2f908c8de8e0a399
Dhriti Dixit <ddhriti61@gmail.com>
2026-05-19 17:09:42 +0000

    Sync LeetCode submission Runtime - 9 ms (86.70%), Memory - 24.1 MB (82.83%)

---
+++ 2634-minimum-common-value/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        i, j = 0, 0
+        len1, len2 = len(nums1), len(nums2)
+        
+        while i < len1 and j < len2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+                
+        return -1
+        
