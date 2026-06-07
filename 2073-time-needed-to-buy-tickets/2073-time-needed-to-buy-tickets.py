f0f0f3687fb6b7f5468091534ad68a08b4010d9e
Dhriti Dixit <ddhriti61@gmail.com>
2026-06-07 23:20:45 +0530

    Time: 11 ms (40.31%), Space: 12.5 MB (20.59%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def timeRequiredToBuy(self, tickets, k):
+        """
+        :type tickets: List[int]
+        :type k: int
+        :rtype: int
+        """
+        time = 0
+        target_tickets = tickets[k]
+        
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[i], target_tickets)
+            else:
+                time += min(tickets[i], target_tickets - 1)
+                
+        return time
