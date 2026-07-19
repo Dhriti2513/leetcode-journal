70676f49ee28fd411cea6fcb3c93c56ea1c7eb17
Dhriti Dixit <ddhriti61@gmail.com>
2026-07-19 18:57:24 +0530

    Time: 127 ms (77.98%), Space: 26.8 MB (95.97%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -1,31 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def createBinaryTree(self, descriptions):
-        """
-        :type descriptions: List[List[int]]
-        :rtype: Optional[TreeNode]
-        """
+class Solution:
+    def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         nodes = {}
         children = set()
         
-        for parent, child, is_left in descriptions:
-            if parent not in nodes:
-                nodes[parent] = TreeNode(parent)
-            if child not in nodes:
-                nodes[child] = TreeNode(child)
+        # Step 1: Build the tree nodes and connect parents to children
+        for parent_val, child_val, is_left in descriptions:
+            if parent_val not in nodes:
+                nodes[parent_val] = TreeNode(parent_val)
+            if child_val not in nodes:
+                nodes[child_val] = TreeNode(child_val)
                 
             if is_left == 1:
-                nodes[parent].left = nodes[child]
+                nodes[parent_val].left = nodes[child_val]
             else:
-                nodes[parent].right = nodes[child]
+                nodes[parent_val].right = nodes[child_val]
                 
-            children.add(child)
+            children.add(child_val)
             
-        for parent, _, _ in descriptions:
-            if parent not in children:
-                return nodes[parent]
+        # Step 2: The root node is the one that never appears as a child
+        for parent_val, _, _ in descriptions:
+            if parent_val not in children:
+                return nodes[parent_val]
